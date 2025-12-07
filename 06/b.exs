d0bc27996798839838b4726139334706098d9b62
Adrian Irving-Beer <wisq@wisq.net>
2025-12-07 14:13:01 -0500

    Add day 6 part 2.

---
+++ 06/b.exs
@@ -0,0 +1,44 @@
+[file] = System.argv()
+
+# Our only two operations are add and multiply.
+# Thus, our tally will consist of 
+#   - a sum (starting at 0)
+#   - a product (starting with 1)
+# and the final line will just choose which one we take.
+initial = {0, 1}
+
+File.stream!(file)
+|> Enum.map(fn line ->
+  line
+  |> :string.chomp()
+  |> String.graphemes()
+end)
+# transpose
+|> Enum.zip_with(&Function.identity/1)
+# start from the rightmost column (now the bottom row)
+|> Enum.reverse()
+|> Enum.reduce({0, initial}, fn row, {total, {sum, product}} ->
+  {operation, digits} = List.pop_at(row, -1)
+  number = Enum.join(digits) |> String.trim()
+
+  {sum, product} =
+    case number do
+      "" ->
+        initial
+
+      n when is_binary(n) ->
+        number = String.to_integer(number)
+        {sum + number, product * number}
+    end
+
+  total =
+    case operation do
+      "*" -> total + product
+      "+" -> total + sum
+      " " -> total
+    end
+
+  {total, {sum, product}}
+end)
+|> elem(0)
+|> IO.inspect()
