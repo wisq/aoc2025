232c3200462e23ec8d7d39fa576e75227fdc2073
Adrian Irving-Beer <wisq@wisq.net>
2025-12-06 22:04:31 -0500

    Add comments and a minor optimisation.

--- 02/b.exs
+++ 02/b.exs
@@ -33,6 +33,28 @@ defmodule InvalidIDs do
     |> Enum.uniq()
   end
 
+  # What ways can we slice up a number into digit groups?
+  #
+  # 123456 -> [{2, 3}, {3, 2}, {4, 1}, {5, 1}, {6, 1}]
+  #   - 2x3 digits, e.g. 100100, 101101, 102102, ...
+  #   - 3x2 digits, e.g. 101010, 111111, 121212, ...
+  #   - 4x1 digits, e.g. 1111, 2222, 3333, ...
+  #   - 5x1 digits, e.g. 11111, 22222, 33333, ...
+  #   - 6x1 digits, e.g. 111111, 222222, 333333, ...
+  #
+  # Note that some combinations won't make any sense --
+  # like if you're testing a range of 99999 to 123456,
+  # then obviously we don't care about 4x1.
+  #
+  # But that's fine, since at just ten cycles,
+  # our 4x1 tests will go from 9999 to 10101010,
+  # which is too high and will stop the 4x1 scan.
+  #
+  # (It's worth noting that `size` is just the START size --
+  #  per above, our 4x1 will eventually turn into a 4x2,
+  #  but as long as our start number has at least 4 digits,
+  #  we'll start with that first digit repeated 4x times
+  #  since the first invalid can't be any lower than that.)
   defp possible_slices(num) do
     str = Integer.to_string(num)
     len = String.length(str)
@@ -44,11 +66,19 @@ defmodule InvalidIDs do
     |> Enum.filter(fn {count, size} -> count > 1 && size > 0 end)
   end
 
+  # Get `size` digits from `num`, assuming `count` groups.
+  #
+  # If `num` has at least `size*count` digits,
+  # this will be the first `size` digits.
+  #
+  # Otherwise, this will be a power of ten with `size` digits.
+  # For a size of 3, that would be 100, because e.g. the lowest
+  # possible 3x3 invalid ID would be 100100100.
   defp leading_digits(num, count, size) do
     str = Integer.to_string(num)
 
     if count * size > String.length(str) do
-      1
+      10 ** (size - 1)
     else
       str
       |> String.slice(0, size)
