6c8af5863461d418f81706a22aeb740a6881d497
Adrian Irving-Beer <wisq@wisq.net>
2025-12-06 19:31:26 -0500

    Cleanup.

--- 01/b.exs
+++ 01/b.exs
@@ -5,7 +5,7 @@ defmodule Dial do
     Enum.reduce(enum, @initial, &parse/2)
   end
 
-  def parse(<<dir, rest::binary>>, state) do
+  defp parse(<<dir, rest::binary>>, state) do
     amount =
       rest
       |> String.trim()
@@ -19,28 +19,28 @@ defmodule Dial do
     |> then(fn {p, z} when p in 0..99 -> {p, z} end)
   end
 
-  def spin_left(amount, {0, zeros}) do
+  defp spin_left(amount, {0, zeros}) do
     (100 - amount)
     |> constrain_left(zeros)
   end
 
-  def spin_left(amount, {pos, zeros}) do
+  defp spin_left(amount, {pos, zeros}) do
     (pos - amount)
     |> constrain_left(zeros)
   end
 
-  def constrain_left(0, z), do: {0, z + 1}
-  def constrain_left(p, z) when p in 1..99, do: {p, z}
-  def constrain_left(p, z) when p in -1..-99, do: {100 + p, z + 1}
-  def constrain_left(p, z) when p < 0, do: constrain_left(rem(p, -100), z + div(p, -100))
+  defp constrain_left(0, z), do: {0, z + 1}
+  defp constrain_left(p, z) when p in 1..99, do: {p, z}
+  defp constrain_left(p, z) when p in -1..-99//-1, do: {100 + p, z + 1}
+  defp constrain_left(p, z) when p < -99, do: constrain_left(rem(p, -100), z + div(p, -100))
 
-  def spin_right(amount, {pos, zeros}) do
+  defp spin_right(amount, {pos, zeros}) do
     (pos + amount)
     |> constrain_right(zeros)
   end
 
-  def constrain_right(p, z) when p in 1..99, do: {p, z}
-  def constrain_right(p, z) when p >= 100, do: {rem(p, 100), z + div(p, 100)}
+  defp constrain_right(p, z) when p in 1..99, do: {p, z}
+  defp constrain_right(p, z) when p >= 100, do: {rem(p, 100), z + div(p, 100)}
 end
 
 [file] = System.argv()
