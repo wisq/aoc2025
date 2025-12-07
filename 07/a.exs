0774e1010f9107d892f3254facabcc9ef7f88d4a
Adrian Irving-Beer <wisq@wisq.net>
2025-12-07 15:12:39 -0500

    Add day 7.

---
+++ 07/a.exs
@@ -0,0 +1,34 @@
+[file] = System.argv()
+
+File.stream!(file)
+|> Enum.with_index()
+|> Enum.reduce(nil, fn
+  {line, 0}, nil ->
+    # First row: Create a beam wherever the "S" starting symbol is.
+    [{col, _}] = Regex.run(~r/S/, line, return: :index)
+    {0, MapSet.new([col])}
+
+  {line, _}, {split_count, beams} ->
+    # Subsequent rows: Find beams hitting splitters.
+    splits =
+      line
+      |> String.trim()
+      |> String.graphemes()
+      |> Enum.with_index()
+      |> Enum.filter(fn
+        {"^", index} -> index in beams
+        {".", _} -> false
+      end)
+
+    {
+      split_count + Enum.count(splits),
+      splits
+      |> Enum.reduce(beams, fn {_, index}, beams ->
+        beams
+        |> MapSet.delete(index)
+        |> MapSet.put(index - 1)
+        |> MapSet.put(index + 1)
+      end)
+    }
+end)
+|> IO.inspect()
